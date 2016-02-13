6948e07cce50037d70bad2fe797674c798e2ee20
Vihan Bhargava <blue1086@gmail.com>
2016-02-12 17:35:45 -0800

    added basic support

--- putt.py
+++ putt.py
@@ -1,17 +1,21 @@
-# I suck at python big time.
-# Please tell me if I can do this better
-
 import urllib2
 import sys
 
 LANGNAME = "Putt" # The language name
 
-def determine(code): # Determines what do with the source code
-  pass
+# Determines what do with the source code
+def determine(code):
+  PYTHON_KEYWORD = "PYTHON" # To run code as Python
+  if code.isdigit():
+    execute(code);
+  elif code.startswith(PYTHON_KEYWORD):
+    exec(code[int(len(PYTHON_KEYWORD)):])
+  else:
+    raise ValueError('%s could not parse the input' % LANGNAME)
 
 if __name__ == '__main__':
   if len(sys.argv) > 1:
-    determine(code)
-    else:
-      print """This is the %s interpreter
-You haven't entered any code.""" % (LANGNAME)
+    determine(sys.argv[1])
+  else:
+    print """This is the %s interpreter
+    You haven't entered any code.""" % (LANGNAME)
